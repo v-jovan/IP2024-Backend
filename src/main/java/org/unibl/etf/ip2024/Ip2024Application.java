eef7e381b6e7c4d62ee902c1def88df329f5fe96
Jovan Vrhovac <jovan.vrhovac@bam.fan>
2024-02-22 19:43:52 +0100

    Initial commit

---
+++ src/main/java/org/unibl/etf/ip2024/Ip2024Application.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ip2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ip2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/unibl/etf/ip2024/Ip2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ip2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
